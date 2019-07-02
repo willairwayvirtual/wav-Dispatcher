d2985c889c3b90feda8d2c9166d4850afd327bbe
Will Jones <willjones97@hotmail.com>
2019-07-02 10:51:44 +0100

    gweggr;'gr

--- wav Virtual Dispatcher/Form2.Designer.vb
+++ wav Virtual Dispatcher/Form2.Designer.vb
@@ -29,7 +29,9 @@ Partial Class Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form2"
         Me.Text = "Form2"
